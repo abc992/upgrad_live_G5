15f9daa33e3da9db7a774c8fadeaac18159c4888
Nishan Ali <nishanali@Nishans-MacBook-Pro.local>
2022-07-30 10:35:39 +0530

    Addition of multiplication

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from summation import sum_
+from multiplication import mult
 
 x = int(input("Enter an Integer:  "))
 y = int(input("Enter another Integer to be added to the previous one:  "))
 
 sum_result = sum_(x, y)
-print(f"Summation of {x} & {y} is: {sum_result}")
+print(f"Summation of {x} & {y} is: [{sum_result}]")
+
+mult_result = mult(x, y)
+print(f"Multiplication of {x} & {y} is: [{mult_result}]")
